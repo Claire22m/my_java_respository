f94f0abe3a75f46f7edaa1f043cbe13e81fcf6da
clairesanez <clairesanez1@gmail.com>
2025-06-19 16:12:10 +0800

    Update FinalLabProject.java

--- FinalLabProject/FinalLabProject.java
+++ FinalLabProject/FinalLabProject.java
@@ -5,10 +5,10 @@ import java.util.Stack;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class CalculatorApp extends JFrame {
+public class FinalLabExam extends JFrame {
     private final JTextField display;
 
-    public CalculatorApp() {
+    public FinalLabExam() {
         setTitle("Java Swing Calculator");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(400, 550);
